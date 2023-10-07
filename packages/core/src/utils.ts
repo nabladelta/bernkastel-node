1e5f80aed60b22692a15bd51a839793501c67a43
a <b>
2023-10-07 22:52:25 +0200

    Remove core library

--- packages/core/src/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from "./protobuf/msgTypes"
-
-export function serializePost(post: IPost) {
-    return Buffer.from(Post.toBinary(Post.create(post)))
-}
-
-export function deserializePost(buf: Buffer): IPost {
-    return Post.clone(Post.fromBinary(buf))
-}
